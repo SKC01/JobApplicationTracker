e5fa073ca5213eb341e79c6849edb10ccd8fc6d2
SKC01 <sakibkader.chowdhury@mnsu.edu>
2024-11-29 23:59:24 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace JobApplication
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+      
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.txtJobTitle = new System.Windows.Forms.TextBox();
+            this.txtJobDescription = new System.Windows.Forms.TextBox();
+            this.txtContactName = new System.Windows.Forms.TextBox();
+            this.txtContactEmail = new System.Windows.Forms.TextBox();
+            this.txtContactPhone = new System.Windows.Forms.TextBox();
+            this.txtNotes = new System.Windows.Forms.TextBox();
+            this.dtpApplicationDate = new System.Windows.Forms.DateTimePicker();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.dtpApplicationDeadline = new System.Windows.Forms.DateTimePicker();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(12, 12);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(200, 20);
+            this.txtCompanyName.TabIndex = 0;
+            this.txtCompanyName.Text = "Company Name";
+            // 
+            // txtJobTitle
+            // 
+            this.txtJobTitle.Location = new System.Drawing.Point(12, 38);
+            this.txtJobTitle.Name = "txtJobTitle";
+            this.txtJobTitle.Size = new System.Drawing.Size(200, 20);
+            this.txtJobTitle.TabIndex = 1;
+            this.txtJobTitle.Text = "Job Title";
+            // 
+            // txtJobDescription
+            // 
+            this.txtJobDescription.Location = new System.Drawing.Point(12, 68);
+            this.txtJobDescription.Name = "txtJobDescription";
+            this.txtJobDescription.Size = new System.Drawing.Size(200, 20);
+            this.txtJobDescription.TabIndex = 2;
+            this.txtJobDescription.Text = "Job Description";
+            // 
+            // txtContactName
+            // 
+            this.txtContactName.Location = new System.Drawing.Point(12, 96);
+            this.txtContactName.Name = "txtContactName";
+            this.txtContactName.Size = new System.Drawing.Size(200, 20);
+            this.txtContactName.TabIndex = 3;
+            this.txtContactName.Text = "Contact Person Name";
+            // 
+            // txtContactEmail
+            // 
+            this.txtContactEmail.Location = new System.Drawing.Point(12, 124);
+            this.txtContactEmail.Name = "txtContactEmail";
+            this.txtContactEmail.Size = new System.Drawing.Size(200, 20);
+            this.txtContactEmail.TabIndex = 4;
+            this.txtContactEmail.Text = "Contact Person Email";
+            // 
+            // txtContactPhone
+            // 
+            this.txtContactPhone.Location = new System.Drawing.Point(12, 152);
+            this.txtContactPhone.Name = "txtContactPhone";
+            this.txtContactPhone.Size = new System.Drawing.Size(200, 20);
+            this.txtContactPhone.TabIndex = 5;
+            this.txtContactPhone.Text = "Contact Person Phone";
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.Location = new System.Drawing.Point(12, 180);
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.Size = new System.Drawing.Size(200, 20);
+            this.txtNotes.TabIndex = 6;
+            this.txtNotes.Text = "Notes";
+            // 
+            // dtpApplicationDate
+            // 
+            this.dtpApplicationDate.Location = new System.Drawing.Point(597, 9);
+            this.dtpApplicationDate.Name = "dtpApplicationDate";
+            this.dtpApplicationDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpApplicationDate.TabIndex = 7;
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Items.AddRange(new object[] {
+            "Applied",
+            "Interview Scheduled",
+            "Offer Received",
+            "Rejected"});
+            this.cmbStatus.Location = new System.Drawing.Point(12, 206);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(200, 21);
+            this.cmbStatus.TabIndex = 8;
+            this.cmbStatus.Text = "Status";
+            // 
+            // dtpApplicationDeadline
+            // 
+            this.dtpApplicationDeadline.Location = new System.Drawing.Point(12, 264);
+            this.dtpApplicationDeadline.Name = "dtpApplicationDeadline";
+            this.dtpApplicationDeadline.Size = new System.Drawing.Size(200, 20);
+            this.dtpApplicationDeadline.TabIndex = 9;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(12, 369);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(200, 30);
+            this.btnSubmit.TabIndex = 10;
+            this.btnSubmit.Text = "Submit Application";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 400);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.dtpApplicationDate);
+            this.Controls.Add(this.txtNotes);
+            this.Controls.Add(this.txtContactPhone);
+            this.Controls.Add(this.txtContactEmail);
+            this.Controls.Add(this.txtContactName);
+            this.Controls.Add(this.txtJobDescription);
+            this.Controls.Add(this.txtJobTitle);
+            this.Controls.Add(this.txtCompanyName);
+            this.Name = "Form1";
+            this.Text = "Job Application Tracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        // Declare the controls
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.TextBox txtJobTitle;
+        private System.Windows.Forms.TextBox txtJobDescription;
+        private System.Windows.Forms.TextBox txtContactName;
+        private System.Windows.Forms.TextBox txtContactEmail;
+        private System.Windows.Forms.TextBox txtContactPhone;
+        private System.Windows.Forms.TextBox txtNotes;
+        private System.Windows.Forms.DateTimePicker dtpApplicationDate;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.DateTimePicker dtpApplicationDeadline;
+        private System.Windows.Forms.Button btnSubmit; // Declare the submit button
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JobApplication
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Server=localhost;Database=JobApplicationTrackerDB;Integrated Security=True;";
+
+        
+        //private DateTimePicker dtpApplicationDeadline;
+
+        private object timerReminder;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Dynamically initialize the DateTimePicker if it's not done in the designer
+            dtpApplicationDeadline = new DateTimePicker();
+            dtpApplicationDeadline.Location = new Point(50, 50);  // Set location as needed
+            dtpApplicationDeadline.Format = DateTimePickerFormat.Short;
+            this.Controls.Add(dtpApplicationDeadline); // Add to the form's controls
+
+        }
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            cmbStatus.Items.Add("Applied");
+            cmbStatus.Items.Add("Interview Scheduled");
+            cmbStatus.Items.Add("Offer Received");
+            cmbStatus.Items.Add("Rejected");
+
+
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            string companyName = txtCompanyName.Text;
+            string jobTitle = txtJobTitle.Text;
+            string jobDescription = txtJobDescription.Text;
+            string contactName = txtContactName.Text;
+            string contactEmail = txtContactEmail.Text;
+            string contactPhone = txtContactPhone.Text;
+            string notes = txtNotes.Text;
+            DateTime applicationDate = dtpApplicationDate.Value; // Get selected date
+            string status = cmbStatus.SelectedItem.ToString();
+
+        }
+
+
+        private void AddJobEntry()
+        {
+            try
+            {
+                // Get values from the form controls
+                string companyName = txtCompanyName.Text;
+                string jobTitle = txtJobTitle.Text;
+                string jobDescription = txtJobDescription.Text;
+                string contactName = txtContactName.Text;
+                string contactEmail = txtContactEmail.Text;
+                string contactPhone = txtContactPhone.Text;
+                string notes = txtNotes.Text;
+                DateTime applicationDate = dtpApplicationDate.Value; // Get selected application date
+                DateTime applicationDeadline = dtpApplicationDeadline.Value; // Get selected deadline date
+                string status = cmbStatus.SelectedItem.ToString();
+
+                // Your logic to save data into the database goes here
+
+                // Example: Insert into database using SqlCommand (adjust query accordingly)
+                string query = @"INSERT INTO JobApplications 
+                         (CompanyName, JobTitle, JobDescription, ContactName, ContactEmail, ContactPhone, Notes, ApplicationDate, ApplicationDeadline, Status) 
+                         VALUES 
+                         (@CompanyName, @JobTitle, @JobDescription, @ContactName, @ContactEmail, @ContactPhone, @Notes, @ApplicationDate, @ApplicationDeadline, @Status)";
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@CompanyName", companyName);
+                        cmd.Parameters.AddWithValue("@JobTitle", jobTitle);
+                        cmd.Parameters.AddWithValue("@JobDescription", jobDescription);
+                        cmd.Parameters.AddWithValue("@ContactName", contactName);
+                        cmd.Parameters.AddWithValue("@ContactEmail", contactEmail);
+                        cmd.Parameters.AddWithValue("@ContactPhone", contactPhone);
+                        cmd.Parameters.AddWithValue("@Notes", notes);
+                        cmd.Parameters.AddWithValue("@ApplicationDate", applicationDate);
+                        cmd.Parameters.AddWithValue("@ApplicationDeadline", applicationDeadline);
+                        cmd.Parameters.AddWithValue("@Status", status);
+
+                        cmd.ExecuteNonQuery(); // Execute the insert query
+                    }
+                }
+                MessageBox.Show("Job application entry added successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnAddJob_Click(object sender, EventArgs e)
+        {
+            // Call the AddJobEntry method when the user clicks the "Add Job" button
+            AddJobEntry();
+        }
+
+        private void UpdateJobStatus(int jobId, string newStatus)
+        {
+            string query = "UPDATE JobApplications SET Status = @Status WHERE Id = @JobId";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Status", newStatus);
+                cmd.Parameters.AddWithValue("@JobId", jobId);
+
+                conn.Open();
+                cmd.ExecuteNonQuery(); // Executes the update command
+            }
+
+            MessageBox.Show("Status updated successfully!");
+        }
+
+        private void CheckReminders()
+        {
+            // Get the application deadline date from the DateTimePicker control
+            DateTime applicationDeadline = dtpApplicationDeadline.Value;
+
+            // Check if the application deadline is today
+            if (applicationDeadline.Date == DateTime.Today)
+            {
+                MessageBox.Show("Reminder: Job application deadline is today!");
+            }
+        }
+
+
+        // Set up a timer to check reminders at regular intervals
+        private void timerReminder_Tick(object sender, EventArgs e)
+        {
+            // 6. Call CheckReminders every time the timer ticks
+            CheckReminders();
+        }
+
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            
+            string companyName = txtCompanyName.Text;
+            string jobTitle = txtJobTitle.Text;
+            string jobDescription = txtJobDescription.Text;
+            string contactName = txtContactName.Text;
+            string contactEmail = txtContactEmail.Text;
+            string contactPhone = txtContactPhone.Text;
+            string notes = txtNotes.Text;
+            DateTime applicationDate = dtpApplicationDate.Value; 
+            DateTime applicationDeadline = dtpApplicationDeadline.Value; 
+            string status = cmbStatus.SelectedItem.ToString(); 
+
+           
+            if (string.IsNullOrEmpty(companyName) || string.IsNullOrEmpty(jobTitle) || string.IsNullOrEmpty(contactName) || string.IsNullOrEmpty(contactEmail))
+            {
+                MessageBox.Show("Please fill in all required fields.");
+                return;
+            }
+
+            // SQL query to insert job application details into the database
+            string query = @"INSERT INTO JobApplications (CompanyName, JobTitle, JobDescription, ContactName, ContactEmail, ContactPhone, Notes, ApplicationDate, ApplicationDeadline, Status) 
+                     VALUES (@CompanyName, @JobTitle, @JobDescription, @ContactName, @ContactEmail, @ContactPhone, @Notes, @ApplicationDate, @ApplicationDeadline, @Status)";
+
+            try
+            {
+                // Database connection and insertion logic
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        
+                        cmd.Parameters.AddWithValue("@CompanyName", companyName);
+                        cmd.Parameters.AddWithValue("@JobTitle", jobTitle);
+                        cmd.Parameters.AddWithValue("@JobDescription", jobDescription);
+                        cmd.Parameters.AddWithValue("@ContactName", contactName);
+                        cmd.Parameters.AddWithValue("@ContactEmail", contactEmail);
+                        cmd.Parameters.AddWithValue("@ContactPhone", contactPhone);
+                        cmd.Parameters.AddWithValue("@Notes", notes);
+                        cmd.Parameters.AddWithValue("@ApplicationDate", applicationDate);
+                        cmd.Parameters.AddWithValue("@ApplicationDeadline", applicationDeadline);
+                        cmd.Parameters.AddWithValue("@Status", status);
+
+                        cmd.ExecuteNonQuery(); // Execute the query to insert data
+                    }
+                }
+
+                // Show success message
+                MessageBox.Show("Job application submitted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // Handle error
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cmbStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadJobData(int jobId)
+        {
+            string query = "SELECT * FROM JobApplications WHERE Id = @JobId"; // Query to select job by Id
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@JobId", jobId); // Pass jobId parameter for filtering
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read()) // Read the data if available
+                            {
+                                // Populate controls with the retrieved data
+                                txtCompanyName.Text = reader["CompanyName"].ToString();
+                                txtJobTitle.Text = reader["JobTitle"].ToString();
+                                txtJobDescription.Text = reader["JobDescription"].ToString();
+                                txtContactName.Text = reader["ContactPersonName"].ToString();
+                                txtContactEmail.Text = reader["ContactPersonEmail"].ToString();
+                                txtContactPhone.Text = reader["ContactPersonPhone"].ToString();
+                                txtNotes.Text = reader["Notes"].ToString();
+                                dtpApplicationDate.Value = Convert.ToDateTime(reader["ApplicationDate"]);
+                                dtpApplicationDeadline.Value = Convert.ToDateTime(reader["ApplicationDeadline"]);
+                                cmbStatus.SelectedItem = reader["Status"].ToString(); // Ensure status matches a combo box option
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        }
+    }
